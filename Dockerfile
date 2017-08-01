257918a12380bfd220bdb8b1350cc948e9220f5d
Jan Garaj <jan.garaj@gmail.com>
2017-08-01 11:39:51 +0100

    V1

--- Dockerfile
+++ Dockerfile
@@ -12,14 +12,14 @@ LABEL maintainer "Jan Garaj <jan.garaj@gmail.com>"
 # https://golang.org/pkg/crypto/tls/
 
 ENV \
-  PORT=443 \
+  PORT=:443 \
   TLS_CERT=/certs/cert.pem \
   TLS_KEY=/certs/key.pem \
   TLS_MIN_VERSION=VersionTLS12 \
-  TLS_CIPHER=TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 \
+  TLS_CIPHER=TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
 
 WORKDIR /
-COPY --from=build /backend /
-COPY certs /certs
-
 ENTRYPOINT ["/showcase-app"]
+COPY certs /certs
+COPY --from=build /showcase-app /
+
--- main.go
+++ main.go
@@ -7,15 +7,22 @@ import (
 	"net/http"
 	"os"
 	"sort"
-  "time"
+	"time"
 )
 
 func main() {
 
+	// startup logs
+	log.Printf("Starting showcase-app on the port %s", os.Getenv("PORT"))
+	for _, pair := range os.Environ() {
+		log.Printf("Env variable: %s\n", pair)
+	}
+
 	name, _ := os.Hostname()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, "Timestamp: %q\n", time.Now().Format(time.RFC850))
+		log.Printf("Processing request")
+		fmt.Fprintf(w, "Timestamp: %q\n", time.Now().Format(time.RFC850))
 		fmt.Fprintf(w, "Hostname: %q\n\n", html.EscapeString(name))
 		keys := make([]string, len(r.Header))
 		i := 0
@@ -33,7 +40,6 @@ func main() {
 
 	})
 
-
-  log.Fatal(http.ListenAndServeTLS(os.Getenv("PORT"), os.Getenv("TLS_CERT"), os.Getenv("TLS_KEY"), nil))
+	log.Fatal(http.ListenAndServeTLS(os.Getenv("PORT"), os.Getenv("TLS_CERT"), os.Getenv("TLS_KEY"), nil))
 
 }
